b215cd0cd776ef1e541ff0cd2a465a70b8a5eff1
Shaun Lazaro <shaunlazaro@gmail.com>
2016-02-26 22:11:28 -0500

    Added camera controller to main scene, flipped cloud
    Camera moves slightly behind, this is intentional.
    Cloud now faces the correct direction for the battle scene.

--- JRPGUnityProject/Assets/Scripts/CameraController.cs
+++ JRPGUnityProject/Assets/Scripts/CameraController.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
+	public GameObject followTarget;
+	public float moveSpeed;
+
+	private Vector3 targetPos;
+
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		targetPos = new Vector3 (followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
+		transform.position = Vector3.Lerp (transform.position, targetPos, moveSpeed * Time.deltaTime);
 	}
 }
