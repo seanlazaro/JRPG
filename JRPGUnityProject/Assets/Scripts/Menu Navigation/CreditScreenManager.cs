4c83487aa937e24910e0d71e5a8d7e5836e630c3
Shaun Lazaro <shaunlazaro@gmail.com>
2016-03-13 15:14:51 -0400

    Added Title Screen Changes
    There may be some sound file thrown in there in prep for next commit

---
+++ JRPGUnityProject/Assets/Scripts/Menu Navigation/CreditScreenManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+public class CreditScreenManager : MonoBehaviour {
+
+    GameObject goToTitleScreen;
+    EventSystem eventSystem;
+
+	// Use this for initialization
+	void Start () {
+	    goToTitleScreen = GameObject.Find("Main Menu Button");
+        GameObject temp = GameObject.Find("EventSystem");
+        eventSystem = temp.GetComponent<EventSystem>();
+	}
+	
+	public void ChangeScene () {
+	    if(eventSystem.currentSelectedGameObject == goToTitleScreen)
+        {
+            SceneManager.LoadScene("TitleMenu");
+        }
+	}
+}
---
+++ JRPGUnityProject/Assets/Scripts/Menu Navigation/TitleScreenManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+public class TitleScreenManager : MonoBehaviour {
+
+    GameObject goToMain;
+    GameObject goToCredits;
+    EventSystem eventSystem;
+
+    void Start()
+    {
+        goToMain = GameObject.Find("Go To Game Button");
+        goToCredits = GameObject.Find("Credits Button");
+        GameObject temp = GameObject.Find("Event System");
+        eventSystem = temp.GetComponent<EventSystem>();
+    }
+    public void ChangeScene()
+    {
+        if(eventSystem.currentSelectedGameObject == goToMain)
+        {
+            SceneManager.LoadScene("main");
+        }
+        else if(eventSystem.currentSelectedGameObject == goToCredits)
+        {
+            SceneManager.LoadScene("credits");
+        }
+
+    }
+}
