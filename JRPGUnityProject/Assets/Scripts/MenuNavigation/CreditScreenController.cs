6756c65985e8180b8cdc97caac1a0d66c8a47702
seanlazaro <seanchristienlazaro@gmail.com>
2016-03-19 18:33:08 -0400

    Improved title and credit scene code

--- JRPGUnityProject/Assets/Scripts/MenuNavigation/CreditScreenController.cs
+++ JRPGUnityProject/Assets/Scripts/MenuNavigation/CreditScreenController.cs
@@ -1,24 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
-using UnityEngine.EventSystems;
 
 public class CreditScreenController : MonoBehaviour {
 
-    GameObject backButton;
-    EventSystem eventSystem;
-
-	// Use this for initialization
-	void Start () {
-	    backButton = GameObject.Find("Back Button");
-        GameObject temp = GameObject.Find("EventSystem");
-        eventSystem = temp.GetComponent<EventSystem>();
-	}
-	
-	public void LoadScene () {
-	    if(eventSystem.currentSelectedGameObject == backButton)
-        {
-            SceneManager.LoadScene("TitleMenu");
-        }
-	}
+    public void OnBackClicked()
+    {
+        SceneManager.LoadScene("TitleMenu");
+    }
 }
--- JRPGUnityProject/Assets/Scripts/MenuNavigation/TitleScreenController.cs
+++ JRPGUnityProject/Assets/Scripts/MenuNavigation/TitleScreenController.cs
@@ -1,31 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
-using UnityEngine.EventSystems;
 
 public class TitleScreenController : MonoBehaviour {
 
-    GameObject startButton;
-    GameObject creditsButton;
-    EventSystem eventSystem;
-
-    void Start()
+    public void OnStartClicked()
     {
-        startButton = GameObject.Find("Go To Game Button");
-        creditsButton = GameObject.Find("Credits Button");
-        GameObject temp = GameObject.Find("Event System");
-        eventSystem = temp.GetComponent<EventSystem>();
+        SceneManager.LoadScene("main");
     }
-    public void ChangeScene()
-    {
-        if(eventSystem.currentSelectedGameObject == startButton)
-        {
-            SceneManager.LoadScene("main");
-        }
-        else if(eventSystem.currentSelectedGameObject == creditsButton)
-        {
-            SceneManager.LoadScene("credits");
-        }
 
+    public void OnCreditsClicked()
+    {
+        SceneManager.LoadScene("Credits");
     }
 }
