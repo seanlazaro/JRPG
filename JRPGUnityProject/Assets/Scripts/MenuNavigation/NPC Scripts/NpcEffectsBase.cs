77304956f03ae558c6470884aacf2a8dd53ed951
seanlazaro <seanchristienlazaro@gmail.com>
2016-08-17 11:47:09 -0400

    Edited NPC Scripts to add extra features
    Edits enable multiple branching conversations and for NPC effects to be
    triggered at any point in the converstaion.

---
+++ JRPGUnityProject/Assets/Scripts/MenuNavigation/NPC Scripts/NpcEffectsBase.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (DialogueController))]
+public abstract class NpcEffectsBase : MonoBehaviour {
+
+    DialogueController dialogueController;
+
+	// Use this for initialization
+	public void Start () {
+		GameObject parent = this.gameObject;
+        dialogueController = parent.GetComponent<DialogueController>();
+	}
+
+	// Update is called once per frame
+	public void Update () {
+
+		if (dialogueController.effectTriggered) {
+            dialogueController.effectTriggered = false;
+
+            int effect = dialogueController.dialogueEffect[dialogueController.CurrentTextIndex];
+
+			StartCoroutine(activateNpcEffect(effect));
+		}
+	}
+
+	public abstract IEnumerator activateNpcEffect(int effect);
+}
---
+++ JRPGUnityProject/Assets/Scripts/MenuNavigation/NPC Scripts/NpcEffectsChangeExitTileDestinations.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (DialogueController))]
+public class NpcEffectsChangeExitTileDestinations : NpcEffectsBase {
+
+	public GameObject[] exitTiles;
+	public string[] destination;
+
+	public override IEnumerator activateNpcEffect(int effect)
+	{
+		// No use for effect variable here because there is only one type of effect
+
+        for (int i = 0; i < exitTiles.Length; i++)
+        {
+            exitTiles[i].GetComponent<ChangeScene>().sceneToLoad = destination[i];
+        }
+		yield break;
+	}
+}
