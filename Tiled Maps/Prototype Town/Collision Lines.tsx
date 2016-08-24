d639283dc45419e647c7f5f95d79b048f8bf6ffb
shaunlazaro <shaunlazaro@gmail.com>
2016-08-23 22:52:12 -0400

    Prototype Town
    Added .tmx files to repo, and added all indoor areas of prototype town.

---
+++ Tiled Maps/Prototype Town/Collision Lines.tsx
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Collision Lines" tilewidth="16" tileheight="16" spacing="1" tilecount="4" columns="2">
+ <image source="../../JRPGUnityProject/Assets/Art/Collision.png" width="33" height="33"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="1" height="16"/>
+   <object id="2" x="0" y="0" width="16" height="1"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="1"/>
+  </objectgroup>
+ </tile>
+</tileset>
